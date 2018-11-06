33e44a9976747015fab51bdfb78794d172705b70
misdan <misdan@localhost.localdomain>
2018-11-06 16:13:29 +0700

    penambahan fungsi untuk mengambil 2 atau lebih logitude

--- src/routes.php
+++ src/routes.php
@@ -32,7 +32,7 @@ $app->get("/sadewa/sst/", function (Request $request, Response $response, $args)
     return $response->withJson(["status" => "success", "data_SST" => $resultData], 200);
 });
 
-
+//fungsi cari data pada file nc
 function cari_data($lat,$lon,$tgl,$jam,$i){
     // Data yang akan dipindah ke python
     $data = array($lat,$lon,$tgl,$jam);
@@ -80,39 +80,39 @@ $app->get("/sadewa/sst/range/lat/", function (Request $request, Response $respon
     }
 });
 
-//dua titik data
-$app->get("/sadewa/sst/range/", function (Request $request, Response $response, $args){
-    /*
-    lat awal harus lebih kecil nilainya dari lat akhir
-    range lat dari -10 sampai 10
-    iterasi ditambah 0.1
-    */
-    $cari_lat_awal = $request->getQueryParam("lat_awal");
-    $cari_lat_akhir = $request->getQueryParam("lat_akhir");
-
-    /*
+//dua titik dengan lat tetap
+$app->get("/sadewa/sst/range/lon/", function (Request $request, Response $response, $args){
+   /*
     lon awal harus lebih kecil nilainya dari lon akhir
     range lon dari 95.0 sampai 145.0
     iterasi ditambah 0.1
     */
+
+    $cari_lat = $request->getQueryParam("lat");
     $cari_lon_awal = $request->getQueryParam("lon_awal");
     $cari_lon_akhir = $request->getQueryParam("lon_akhir");
 
     $cari_tgl = $request->getQueryParam("tgl");
     $cari_jam = $request->getQueryParam("jam");
 
-    /*
-    // Data yang akan dipindah ke python
-    $data = array($cari_lat,$cari_lon,$cari_tgl,$cari_jam);
+    //conversi data ke float
+    $lon_1 = floatval($cari_lon_awal);
+    $lon_2 = floatval($cari_lon_akhir);
 
-    // Execute the python script with the JSON data
-    $result = shell_exec('python "/var/www/html/lapan-api/src/API/SST_Sadewa.py" ' . base64_encode(json_encode($data)));
+    //loop
+    $i=1;
+    while ($lon_1<=$lon_2) {
+        //cari data dengan fungsi
+        $data = cari_data($cari_lat,$lon_1,$cari_tgl,$cari_jam,$i);
+       
+        //var_dump ($data);
+        $myJSON = json_encode(["status" => "success", "data_SST_Ke_".$i => $data],200);
+        echo $myJSON;
 
-    // Decode the result
-    $resultData = json_decode($result, true);
-
-    return $response->withJson(["status" => "success", "data_SST" => $resultData], 200);
-    */
+         //iterasi
+        $i=$i+1;
+        $lon_1 = $lon_1+0.1;
+    }
 });
 
 
